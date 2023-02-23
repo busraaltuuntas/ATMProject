510b3d36f70510e7faef53c6f8851dc1aafa8997
Büşra Altuntaş <99415208+busraaltuuntas@users.noreply.github.com>
2023-02-24 00:49:32 +0300

    JAVA

---
+++ ATMproject/src/Main.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String userName, password;
+        Scanner input = new Scanner(System.in);
+        int right=3;
+        int balance = 3500;
+        int select;
+        while (right>0) {
+            System.out.print("Kullanıcı adınızı giriniz:");
+            userName = input.nextLine();
+            System.out.println("Paralonız:");
+            password = input.nextLine();
+            if(userName.equals("blue") && (password.equals("shine"))){
+                System.out.println("Merhaba X Bankasına Hoşgeldiniz");
+                do{
+                    System.out.println("1-Para Yatırm\n" +
+                            "2-Para Çekme\n" +
+                            "3-Bakiye Sorgulama\n" +
+                            "4-Çıkış Yap");
+                    System.out.print("Lütfen yapmak istediğiniz işlemi seçiniz:");
+                    select = input.nextInt();
+                    if(select==1){
+                        System.out.print("Para miktarı:");
+                        int price = input.nextInt();
+                        balance += price;
+                    } else if (select==2) {
+                        System.out.println("Para miktarı");
+                        int price = input.nextInt();
+                        if(price > balance){
+                            System.out.println("Bakiyeniz yetersiz.");
+                        }else{
+                            balance-=price;
+                        }
+                    } else if (select==3) {
+                        System.out.println("Bakiyeniz:"+ balance);
+
+                    }
+                }while(select != 4);
+                System.out.println("Tekrar görüşmek üzere");
+                break;
+            }else {
+                right--;
+                System.out.println("Hatalı kullanıcı adı veya şifre. Tekrar deneyiniz");
+                if(right==0){
+                    System.out.println("Hesabınız bloke olmuştur lütfen banka ile iletişime geçiniz");
+                }else{
+                    System.out.println("Kalan hakkınız:" + right );
+
+                }
+
+            }
+
+        }
+    }
+}
